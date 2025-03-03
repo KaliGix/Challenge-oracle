032e8bbf9811858ef585dba636bd720c8d4c8666
KaliGix <kalibell19dalmasi@gmail.com>
2025-03-03 15:42:18 -0400

    [Update] The function drawFriend() was added

--- app.js
+++ app.js
@@ -6,16 +6,30 @@ function addFriend(){
     let inputFriend = document.getElementById("friend");
 
     if(inputFriend.value !=""){ //check if the input is not empty
+        
         friends.push(inputFriend.value);//add the friend to the array
-        clearInput(inputFriend);//clear the input field
+        inputFriend.value = ""; //clear the input field
         console.log("Friend added successfully"); // log the success message
         console.log(friends); // log the array of friends
+        return;
     } 
     else{
         alert("Please enter a valid name");
+        return;
     }
 }
+// Function to draw a friend randomly
+function drawFriend(){
 
-function clearInput(input){
-   input.value = "";
+    if(friends.length > 0){ //check if the array is not empty
+
+        let randomFriend = Math.floor(Math.random() * friends.length);// get a random friend from the array
+        let friendWinner = friends[randomFriend]; // get the name of the winner
+        document.querySelector('#result').innerHTML = friendWinner; // display the name of the winner
+        console.log(friendWinner);
+    }
+    else {
+        alert("Please add a friend first");
+        return;
+    }
 }
