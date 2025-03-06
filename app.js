309b69ae574e0273431fb4316377326bed00dde1
Kalibell Dalmasi <kalibelldalmasi@gmail.com>
2025-03-05 21:50:34 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let friends = []; // Array of friends to be added
 
 function addFriend(){
